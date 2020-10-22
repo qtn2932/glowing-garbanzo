f0f300bcc8d3e79bc86679162290e1f437495368
Quan Nguyen <QuanNguyen@Quans-Air.fios-router.home>
2020-10-21 22:28:31 -0400

    update mk

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def index():
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,3)
-    loaded_model = pickle.load(open('./model.pkl','rb'))
+    loaded_model = pickle.load(open('model.pkl','rb'))
     result = loaded_model.predict(to_predict)
     return result[0]
 
